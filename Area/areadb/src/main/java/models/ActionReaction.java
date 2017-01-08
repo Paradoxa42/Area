0a6f4f7b688ff48351c694b9b9b2c2aa15904310
dahlaran <joseph.demerss@hotmail.fr>
2017-01-08 18:34:14 +0100

    correction du précendant

---
+++ Area/areadb/src/main/java/models/ActionReaction.java
@@ -0,0 +1,40 @@
+package models;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.google.common.collect.Lists;
+import com.sun.org.apache.xerces.internal.impl.dv.xs.BooleanDV;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+/**
+ * Created by Gabriel on 06/01/2017.
+ */
+
+@Entity
+public class ActionReaction
+{
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    public Long id_client;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    public Long id_action;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    public Long id_reaction;
+
+    public ActionReaction ()  {}
+}
